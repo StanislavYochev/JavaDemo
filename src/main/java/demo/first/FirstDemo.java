1a8e6ebfd9456648a55fd385c6852c1238b2360a
DESKTOP-DLHAJ9J\PC <stanislavyochev1994r@gmail.com>
2023-06-22 12:33:06 +0300

    Initial commit

---
+++ src/main/java/demo/first/FirstDemo.java
@@ -0,0 +1,12 @@
+//defenition of package
+package demo.first;
+
+//defebutuib of a class
+public class FirstDemo {
+
+    //method
+    public static void main (String [] args){
+    System.out.println("Hello Java");
+    }
+
+}
